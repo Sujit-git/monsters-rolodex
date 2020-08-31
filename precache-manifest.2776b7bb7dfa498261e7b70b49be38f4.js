bc528d9eaf94fd231cd4d801e3ed0db9332a47a5
Sujit <sujitkhopade2001@gmail.com>
2020-08-31 01:03:24 -0700

    Updates

---
+++ precache-manifest.2776b7bb7dfa498261e7b70b49be38f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f56af4f3fc286182d8035d88f7cf5dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b90144ac7848f844dcea",
+    "url": "/monsters-rolodex/static/css/main.247fbb11.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90144ac7848f844dcea",
+    "url": "/monsters-rolodex/static/js/main.6a5b1dd1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
